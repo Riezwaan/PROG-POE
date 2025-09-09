d3a447d3dc0b4e07cad9ea069c69486f5bc83845
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-09-09 20:24:36 +0000

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace PROG_POE
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
